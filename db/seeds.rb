54ba92e3a00cc212b27282eb89e47cb06ae9022a
Rafael Pierri <rpierri@thoughtworks.com>
2016-10-21 10:01:50 -0200

    Factory default industries

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,26 @@
 #   movies =
 #   Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+Industry.create([
+  { name: 'Agricultura e Mineração' },
+  { name: 'Serviços Empresariais' },
+  { name: 'Computadores e Eletrônicos' },
+  { name: 'Serviços de Consumo' },
+  { name: 'Educação' },
+  { name: 'Energia e Serviços Públicos' },
+  { name: 'Engenharia, Pesquisa e Desenvolvimento' },
+  { name: 'Serviços Financeitos e Seguradoras' },
+  { name: 'Governo' },
+  { name: 'Assistência Médica, Industria Farmacêutica e Biotecnologia' },
+  { name: 'Manufatura' },
+  { name: 'Mídia, Publicação e Entretenimento' },
+  { name: 'Sem Finalidade Lucrativa' },
+  { name: 'Mercado Imobiliário e Construções' },
+  { name: 'Varejo' },
+  { name: 'Software e Internet' },
+  { name: 'Telecomunicações' },
+  { name: 'Transporte e Estocagem' },
+  { name: 'Viagem, Lazer e Recreação' },
+  { name: 'Atacado e Distribuição' },
+  { name: 'Outro' }])
