f2d69273a2f397634610505403df5bc69fe6629f
Carlos Maniero <cmaniero@thoughtworks.com>
2016-10-24 11:29:42 -0200

    Carlos/Gabi - Implemented user cookies for start controller

--- spec/javascripts/app/users/controller_spec.js
+++ spec/javascripts/app/users/controller_spec.js
@@ -1,43 +1,94 @@
-describe('UserService', function() {
-    var $scope, StartController, UserService, $controller, $rootScope;
+describe('StartController', function() {
+    var $scope, StartController, UserService, $controller, $rootScope, $cookies, $location;
     // Set the module
     beforeEach(module('trendOMeterApp'));
 
     // Add globals for any test
-    beforeEach(inject(function(_$rootScope_, _$controller_, _UserService_) {
+    beforeEach(inject(function(_$rootScope_, _$controller_, _UserService_, _$cookies_, _$location_) {
         UserService = _UserService_;
         $controller = _$controller_;
         $rootScope = _$rootScope_;
         $scope = $rootScope.$new();
+        $cookies = _$cookies_;
+        $location = _$location_;
+        StartController = $controller('StartController', {$scope: $scope});
     }));
 
     it('should set loading equal true on $scope.start() is called', function(){
-        StartController = $controller('StartController', {$scope: $scope});
         expect($scope.loading).toEqual(false);
         $scope.start();
         expect($scope.loading).toEqual(true);
-        // expect(UserService.createAnonymous.calls.count()).toEqual(1);
     });
 
-    it('should set loading equal false on creating a anonymous user', function(){
-        spyOn(UserService, "createAnonymous").and.returnValue({
-            then: function(fn){
-                // Check if is loading before response the promise.
-                expect($scope.loading).toEqual(true);
+    describe('UserService.createAnonymous', function(){
+        beforeEach(function(){
+            spyOn(UserService, "createAnonymous").and.returnValue({
+                then: function(fn){
+                    // Check if is loading before response the promise.
+                    expect($scope.loading).toEqual(true);
 
-                return fn({
-                    status: 201,
-                    data: {
-                        id: 1,
-                        anonym: true
-                    }
-                })
-            }
+                    return fn({
+                        status: 201,
+                        data: {
+                            id: 1,
+                            anonym: true
+                        }
+                    })
+                }
+            });
         });
-        // Init the controller
-        StartController = $controller('StartController', {$scope: $scope});
-        $scope.start();
-        expect(UserService.createAnonymous.calls.count()).toEqual(1);
-        expect($scope.loading).toEqual(false);
+
+        afterEach(function() {
+            $cookies.remove('user_id');
+        });
+
+        it('should set loading equal false on creating a anonymous user', function(){
+            // Init the controller
+            $scope.start();
+            expect(UserService.createAnonymous.calls.count()).toEqual(1);
+            expect($scope.loading).toEqual(false);
+        });
+       
+        it('should not call createAnonymous when given a cookie before', function() {
+            $cookies.put('user_id',1);
+            $scope.start();
+            expect(UserService.createAnonymous.calls.count()).toEqual(0);
+        });
+
+        it('should call redirect after starting', function() {
+            spyOn($scope, "redirect").and.callThrough();
+            $scope.start();
+            expect($scope.redirect.calls.count()).toEqual(1);
+        });
+
+        it('should redirect to /duel on redirect call', function() {
+            $scope.redirect();
+            expect($location.path()).toEqual('/duel');
+        });
+    });
+
+    describe('UserService.createAnonymous invalid', function(){
+        beforeEach(function(){
+            spyOn(UserService, "createAnonymous").and.returnValue({
+                then: function(fn){
+                    // Check if is loading before response the promise.
+                    expect($scope.loading).toEqual(true);
+
+                    return fn({
+                        status: 500,
+                        data: {
+                            id: 1,
+                            anonym: true
+                        }
+                    })
+                }
+            });
+        });
+
+        it('should show error message', function() {
+            expect($scope.error).toBeFalsy();
+            $scope.start();
+            expect($scope.error).toBeTruthy();
+        });
     });
 });
