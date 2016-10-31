f18256b5c47bcd5e2bfc94d89e02eb97fd36d530
Rafael Pierri <rpierri@thoughtworks.com>
2016-10-31 18:42:04 -0200

    Rafael - Panel screen layout

--- app/assets/javascripts/app/panel/controllers.js
+++ app/assets/javascripts/app/panel/controllers.js
@@ -3,7 +3,22 @@ trendOMeterApp.controller('PanelController', function($scope, PanelService, $loc
 
     function init() {
         PanelService.getTrends().then(function(response) {
-            $scope.panelTrends = response.data;
+            $scope.trends = {}
+            if(response.data.length > 5){
+                $scope.trends.first = response.data[0];
+                $scope.trends.second = response.data[1];
+                $scope.trends.third = response.data[2];
+                $scope.trends.antepenult = response.data[3];
+                $scope.trends.penultimate = response.data[4];
+                $scope.trends.last = response.data[5];
+            }else{
+                $scope.trends.first = response.data[0];
+                $scope.trends.second = response.data[1];
+                $scope.trends.third = response.data[2];
+                $scope.trends.antepenult = response.data[response.data.length - 3];
+                $scope.trends.penultimate = response.data[response.data.length - 2];
+                $scope.trends.last = response.data[response.data.length - 1];
+            } 
             $scope.loading = false;
         });
     }
