5570028ec57c8511dda3a0784a135557d8000830
Rafael Pierri <rpierri@thoughtworks.com>
2016-11-01 17:37:26 -0200

    Rafael/Gabi - Fixed logic for result page

--- app/assets/javascripts/app/panel/controllers.js
+++ app/assets/javascripts/app/panel/controllers.js
@@ -1,23 +1,33 @@
 trendOMeterApp.controller('PanelController', function($scope, PanelService, $location, $timeout) {
     $scope.loading = true; 
 
+    function addPosition(data){
+        var array = [];
+        data.map(function(item){
+            item['position'] = data.indexOf(item) + 1;
+            array.push(item);
+        });
+        return array;
+    }
     function init() {
         PanelService.getTrends().then(function(response) {
+            var data = addPosition(response.data);
             $scope.trends = {}
-            if(response.data.length > 5){
-                $scope.trends.first = response.data[0];
-                $scope.trends.second = response.data[1];
-                $scope.trends.third = response.data[2];
-                $scope.trends.antepenult = response.data[3];
-                $scope.trends.penultimate = response.data[4];
-                $scope.trends.last = response.data[5];
+            if(response.data.length < 7){
+                $scope.trends.first = data[0];
+                $scope.trends.second = data[1];
+                $scope.trends.third = data[2];
+                $scope.trends.antepenult = data[3];
+                $scope.trends.penultimate = data[4];
+                $scope.trends.last = data[5];
             }else{
-                $scope.trends.first = response.data[0];
-                $scope.trends.second = response.data[1];
-                $scope.trends.third = response.data[2];
-                $scope.trends.antepenult = response.data[response.data.length - 3];
-                $scope.trends.penultimate = response.data[response.data.length - 2];
-                $scope.trends.last = response.data[response.data.length - 1];
+                $scope.trends.first = data[0];
+                $scope.trends.second = data[1];
+                $scope.trends.third = data[2];
+                $scope.trends.antepenult = data[data.length - 3];
+                $scope.trends.penultimate = data[data.length - 2];
+                $scope.trends.last = data[data.length - 1];
+                $scope.trends.split = true;
             } 
             $scope.loading = false;
         });
