1d84fa04d83523c2a62b28a7763af13a90405c45
Rafael Pierri <rpierri@thoughtworks.com>
2016-11-01 20:08:54 -0200

    Rafael/Gabi - thank you screen finalization

--- app/assets/javascripts/app/thanks/controller.js
+++ app/assets/javascripts/app/thanks/controller.js
@@ -1,3 +1,10 @@
 trendOMeterApp.controller('ThanksController', function($scope, $location, $timeout) {
+    $scope.start = function() {
+        $location.path('/duels');
+    }
+
+    $scope.panel = function() {
+        $location.path('/panel');
+    }
 });
 
