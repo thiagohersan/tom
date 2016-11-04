e0d77da0a9748b707ab55960e27f43a3f323caa5
Gabrielle Saman <gsaman@thoughtworks.com>
2016-11-04 11:49:06 -0200

    Gabi - Sorry for the red build

--- spec/controllers/emails_controller_spec.rb
+++ spec/controllers/emails_controller_spec.rb
@@ -10,11 +10,11 @@ RSpec.describe EmailsController, type: :controller do
     it "stores a new user in the database"do
       user = create(:user)
       post :find, email: user.email
-      expect(assigns(:result)).to eq(true)
+      expect(assigns(:result)).to eq("Sim")
     end
     it "returns false if email does not exist"do
       post :find, email: 'crazy@mail.com' 
-      expect(assigns(:result)).to eq(false)
+      expect(assigns(:result)).to eq("Não")
     end
   end
 end
