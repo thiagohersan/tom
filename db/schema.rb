5cadb1083c0236d303d495551b144463d87674d8
Patrick Maia <patrickjourdanmaia@gmail.com>
2017-10-24 17:22:47 -0200

    adds migration to add image field to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161212133733) do
+ActiveRecord::Schema.define(version: 20171024162232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20161212133733) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.boolean  "anonymous"
+    t.string   "image"
     t.index ["industry_id"], name: "index_users_on_industry_id", using: :btree
     t.index ["role_id"], name: "index_users_on_role_id", using: :btree
   end
