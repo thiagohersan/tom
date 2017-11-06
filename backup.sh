3fcc792d76d322f99367ba725e1986bc9032af91
Dayany Espindola <dayanyrec@gmail.com>
2017-11-06 16:21:19 -0200

    adds backup script

---
+++ backup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+TIMESTAMP=`date +"%s"`
+BACKUP_FOLDER=$DEST/$TIMESTAMP
+mkdir $BACKUP_FOLDER
+echo "backup folder is $BACKUP_FOLDER"
+docker-compose exec postgres_host pg_dump -U docker trend > $BACKUP_FOLDER/database.sql
+cp ./public/images/system/* $BACKUP_FOLDER
