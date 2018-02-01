dfaef8ea08188efe3930a3e33bdc45f76d234710
Thiago Hersan <thiago.hersan+github@gmail.com>
2018-02-01 13:33:04 -0200

    skips business card image capture

--- app/assets/javascripts/app/users/controllers.js
+++ app/assets/javascripts/app/users/controllers.js
@@ -4,7 +4,7 @@ trendOMeterApp.controller('StartController', function($location, $scope, UserSer
     $scope.isCompleted = UserService.isCompleted();
 
     $scope.redirect = function(){
-        $location.path('/capture');
+        $location.path('/duels');
     }
 
     $scope.panel = function() {
